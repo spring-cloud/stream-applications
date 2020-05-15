6c83ae2fa08be4379ae67aba44df6ffc72f73704
Soby Chacko <schacko@pivotal.io>
2020-05-15 15:53:06 -0400

    Counter sink test cleanup

--- applications/sink/counter-sink/src/test/java/org/springframework/cloud/stream/app/sink/counter/CounterSinkTests.java
+++ applications/sink/counter-sink/src/test/java/org/springframework/cloud/stream/app/sink/counter/CounterSinkTests.java
@@ -67,7 +67,6 @@ public class CounterSinkTests {
 	}
 
 	@EnableAutoConfiguration
-	//@Import({CounterConsumerConfiguration.class, PayloadConverterConfiguration.class})
 	@Import({CounterConsumerConfiguration.class})
 	public static class CounterSinkTestConfiguration {
 	}
