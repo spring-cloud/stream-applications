c598674a986ae030cbe317ea07a925ecd4a05d5a
Soby Chacko <schacko@pivotal.io>
2020-05-18 16:11:44 -0400

    Adding script-processor apps
    https://github.com/spring-cloud/stream-applications/issues/34
    
    * Addressing PR review comments

---
+++ applications/processor/script-processor/src/test/resources/script-with-grab.groovy
@@ -0,0 +1,6 @@
+@Grab('org.grooscript:grooscript:1.3.0')
+import org.grooscript.GrooScript
+@Grab('org.grooscript:grooscript:1.3.0')
+import org.grooscript.GrooScript
+
+GrooScript.convert(new String(payload))
