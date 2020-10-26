bba1974582e51aef80b3890299f87a3e809d9847
David Turanski <dturanski@vmware.com>
2020-10-26 15:12:23 -0400

    Switch template to use jupiter.

--- spring-cloud-dataflow-apps-generator-plugin/src/main/resources/template/AppTests.java
+++ spring-cloud-dataflow-apps-generator-plugin/src/main/resources/template/AppTests.java
@@ -16,12 +16,9 @@
 
 package {{app-package-name}};
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class {{app-class-name}}Tests {
 
