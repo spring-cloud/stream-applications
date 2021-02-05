547d87afce4d0bef0a40eef03b7c3fe04943a684
Artem Bilan <abilan@vmware.com>
2021-02-05 12:56:17 -0500

    * Fix Checkstyle violations * Update `README.adoc` for `mongodb-source`

--- functions/supplier/mongodb-supplier/src/test/java/org/springframework/cloud/fn/supplier/mongo/MongodbSupplierApplicationTests.java
+++ functions/supplier/mongodb-supplier/src/test/java/org/springframework/cloud/fn/supplier/mongo/MongodbSupplierApplicationTests.java
@@ -42,7 +42,7 @@ import static org.assertj.core.api.Assertions.entry;
 		"spring.data.mongodb.port=0",
 		"mongodb.supplier.collection=testing",
 		"mongodb.supplier.query={ name: { $exists: true }}",
-		"mongodb.supplier.update-expression='{ $unset: { name: 0 } }'",
+		"mongodb.supplier.update-expression='{ $unset: { name: 0 } }'"
 })
 class MongodbSupplierApplicationTests {
 
