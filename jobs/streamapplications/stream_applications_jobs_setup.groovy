373870d5ebf0d665cbaffda7ec97eb6c9fd3f052
Soby Chacko <schacko@pivotal.io>
2022-01-21 14:13:14 -0500

    Adding builds for 2021.0.x apps branch

--- jobs/streamapplications/stream_applications_jobs_setup.groovy
+++ jobs/streamapplications/stream_applications_jobs_setup.groovy
@@ -10,5 +10,8 @@ String releaseType = "" // possible values are - "", milestone or ga
 // Main CI
 new StreamApplicationsPhasedBuildMaker(dsl).build(false, "")
 
+// 2021.0.x CI
+new StreamApplicationsPhasedBuildMaker(dsl).build(false, "", "2021.0.x")
+
 // 2020.0.x CI
 new StreamApplicationsPhasedBuildMaker(dsl).build(false, "", "2020.0.x")
