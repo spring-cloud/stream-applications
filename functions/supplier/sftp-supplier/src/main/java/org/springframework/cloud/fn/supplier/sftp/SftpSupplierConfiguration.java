5d1127bbd121208954bbeb576051451e6ba2e184
onobc <chris.bono@gmail.com>
2022-02-08 19:20:03 -0600

    Fix checkstyle on previous commit

--- functions/supplier/sftp-supplier/src/main/java/org/springframework/cloud/fn/supplier/sftp/SftpSupplierConfiguration.java
+++ functions/supplier/sftp-supplier/src/main/java/org/springframework/cloud/fn/supplier/sftp/SftpSupplierConfiguration.java
@@ -21,7 +21,6 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.jcraft.jsch.ChannelSftp.LsEntry;
