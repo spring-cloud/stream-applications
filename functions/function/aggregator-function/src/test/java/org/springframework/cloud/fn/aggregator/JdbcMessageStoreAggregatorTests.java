ed88fa6c4cde7c599b18f19880fe573d5cf3a427
Artem Bilan <abilan@vmware.com>
2022-03-28 13:54:59 -0400

    GH-248: Exclude Geode SSL config
    Fixes https://github.com/spring-cloud/stream-applications/issues/248
    
    The Geode SSL auto-config is done via `SslAutoConfiguration.SslEnvironmentPostProcessor`
    which cannot be excluded as an auto-config.
    
    * Set `SslAutoConfiguration.SECURITY_SSL_ENVIRONMENT_POST_PROCESSOR_ENABLED_PROPERTY`
    to `false` from the `ExcludeStoresAutoConfigurationEnvironmentPostProcessor`
    when the store type is not Geode

--- functions/function/aggregator-function/src/test/java/org/springframework/cloud/fn/aggregator/JdbcMessageStoreAggregatorTests.java
+++ functions/function/aggregator-function/src/test/java/org/springframework/cloud/fn/aggregator/JdbcMessageStoreAggregatorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2020 the original author or authors.
+ * Copyright 2020-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,6 +67,8 @@ public class JdbcMessageStoreAggregatorTests extends AbstractAggregatorFunctionT
 		assertThat(this.messageGroupStore).isInstanceOf(JdbcMessageStore.class);
 
 		assertThat(this.aggregatingMessageHandler.getMessageStore()).isSameAs(this.messageGroupStore);
+
+		assertThat(this.geodeSslEnable).isFalse();
 	}
 
 }
