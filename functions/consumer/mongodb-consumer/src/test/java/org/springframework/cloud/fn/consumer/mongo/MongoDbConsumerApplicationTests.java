bb0e82c08e99fd2013b5d958584092d9dd06b0d0
Chris Bono <chris.bono@gmail.com>
2022-10-02 09:49:10 -0500

    Fix mongodb-consumer
    Replace embedded MongoDB w/ MongoDB testcontainer

--- functions/consumer/mongodb-consumer/src/test/java/org/springframework/cloud/fn/consumer/mongo/MongoDbConsumerApplicationTests.java
+++ functions/consumer/mongodb-consumer/src/test/java/org/springframework/cloud/fn/consumer/mongo/MongoDbConsumerApplicationTests.java
@@ -33,17 +33,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 /**
  * @author David Turanski
+ * @author Chris Bono
  */
 @SpringBootTest(properties = {
-		"mongodb.consumer.collection=testing",
-		"spring.mongodb.embedded.version=3.5.5" })
-class MongoDbConsumerApplicationTests {
+		"mongodb.consumer.collection=testing"
+})
+class MongoDbConsumerApplicationTests implements MongoDbTestContainerSupport {
+
+	@DynamicPropertySource
+	static void mongoDbProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.mongodb.uri", MongoDbTestContainerSupport::mongoDbUri);
+		registry.add("spring.data.mongodb.database", () -> "test");
+	}
 
 	@Autowired
 	private MongoDbConsumerProperties properties;
