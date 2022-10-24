3d489991b1cc8d166276e77409beaa73d0445e7c
Artem Bilan <abilan@vmware.com>
2022-10-24 18:19:33 -0400

    GH-350: Upgrade SI Cassandra; re-enable tests (#381)
    * GH-350: Upgrade SI Cassandra; re-enable tests
    
    Fixes https://github.com/spring-cloud/stream-applications/issues/350
    
    * Use Testcontainers for Cassandra
    * Adjust components code style a bit
    * Convert `Book` testing entity to the `record`
    * Use `@DynamicPropertySource` to expose Docker ENV to Spring Boot configuration properties
    * Use switch expression for better code readability

--- functions/consumer/cassandra-consumer/src/test/java/org/springframework/cloud/fn/consumer/cassandra/CassandraConsumerApplicationTests.java
+++ functions/consumer/cassandra-consumer/src/test/java/org/springframework/cloud/fn/consumer/cassandra/CassandraConsumerApplicationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,41 +19,35 @@ package org.springframework.cloud.fn.consumer.cassandra;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
 
-import org.cassandraunit.spring.CassandraUnitDependencyInjectionIntegrationTestExecutionListener;
-import org.cassandraunit.spring.EmbeddedCassandra;
-import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
+
 import reactor.core.publisher.Mono;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.fn.consumer.cassandra.domain.Book;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.data.cassandra.core.WriteResult;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 /**
  * @author Artem Bilan
  */
-@Disabled
-@TestExecutionListeners(mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS,
-		listeners = CassandraUnitDependencyInjectionIntegrationTestExecutionListener.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
 		properties = {
-				"spring.data.cassandra.keyspaceName=" + CassandraConsumerApplicationTests.CASSANDRA_KEYSPACE,
-				"spring.data.cassandra.localDatacenter=datacenter1",
+				"spring.cassandra.keyspace-name=" + CassandraConsumerApplicationTests.CASSANDRA_KEYSPACE,
 				"cassandra.cluster.createKeyspace=true" })
-@EmbeddedCassandra(configuration = EmbeddedCassandraServerHelper.CASSANDRA_RNDPORT_YML_FILE, timeout = 120000)
 @DirtiesContext
-abstract class CassandraConsumerApplicationTests {
+abstract class CassandraConsumerApplicationTests implements CassandraContainerTest {
 
 	static final String CASSANDRA_KEYSPACE = "test";
 
@@ -63,16 +57,14 @@ abstract class CassandraConsumerApplicationTests {
 	@Autowired
 	protected Function<Object, Mono<? extends WriteResult>> cassandraConsumer;
 
-	@BeforeAll
-	static void setUp() {
-		EmbeddedCassandraServerHelper.getSession();
-		System.setProperty("spring.data.cassandra.contactPoints",
-				EmbeddedCassandraServerHelper.getHost() + ':' + EmbeddedCassandraServerHelper.getNativeTransportPort());
-	}
 
-	@AfterAll
-	static void cleanup() {
-		System.clearProperty("spring.data.cassandra.contactPoints");
+	@DynamicPropertySource
+	static void registerConfigurationProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.cassandra.localDatacenter", () -> CASSANDRA_CONTAINER.getLocalDatacenter());
+		registry.add("spring.cassandra.contactPoints", () ->
+			Optional.of(CASSANDRA_CONTAINER.getContactPoint())
+					.map(contactPoint -> contactPoint.getAddress().getHostAddress() + ':' + contactPoint.getPort())
+					.get());
 	}
 
 	@AfterEach
@@ -83,23 +75,23 @@ abstract class CassandraConsumerApplicationTests {
 	protected static List<Book> getBookList(int numBooks) {
 
 		List<Book> books = new ArrayList<>();
-
-		Book b;
 		for (int i = 0; i < numBooks; i++) {
-			b = new Book();
-			b.setIsbn(UUID.randomUUID());
-			b.setTitle("Spring Cloud Data Flow Guide");
-			b.setAuthor("SCDF Guru");
-			b.setPages(i * 10 + 5);
-			b.setInStock(true);
-			b.setSaleDate(LocalDate.now());
-			books.add(b);
+			books.add(
+					new Book(
+							UUID.randomUUID(),
+							"Spring Cloud Data Flow Guide",
+							"SCDF Guru",
+							i * 10 + 5,
+							LocalDate.now(),
+							true));
 		}
 
 		return books;
 	}
 
-	@SpringBootApplication
+	@SpringBootConfiguration
+	@EnableAutoConfiguration
+	@Import(CassandraConsumerConfiguration.class)
 	static class CassandraConsumerTestApplication {
 
 	}
--- functions/consumer/cassandra-consumer/src/test/java/org/springframework/cloud/fn/consumer/cassandra/domain/Book.java
+++ functions/consumer/cassandra-consumer/src/test/java/org/springframework/cloud/fn/consumer/cassandra/domain/Book.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package org.springframework.cloud.fn.consumer.cassandra.domain;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import org.springframework.data.cassandra.core.mapping.Indexed;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
@@ -29,118 +30,12 @@ import org.springframework.data.cassandra.core.mapping.Table;
  * @author Artem Bilan
  */
 @Table("book")
-public class Book {
-
-	@PrimaryKey
-	private UUID isbn;
-
-	private String title;
-
-	private String author;
-
-	private int pages;
-
-	private LocalDate saleDate;
-
-	private boolean inStock;
-
-	public Book() {
-	}
-
-	public Book(UUID isbn, String title, String author) {
-		this.isbn = isbn;
-		this.title = title;
-		this.author = author;
-	}
-
-	/**
-	 * @return Returns the isbn.
-	 */
-	public UUID getIsbn() {
-		return this.isbn;
-	}
-
-	/**
-	 * @return Returns the saleDate.
-	 */
-	public LocalDate getSaleDate() {
-		return this.saleDate;
-	}
-
-	/**
-	 * @param saleDate The saleDate to set.
-	 */
-	public void setSaleDate(LocalDate saleDate) {
-		this.saleDate = saleDate;
-	}
-
-	/**
-	 * @return Returns the inStock.
-	 */
-	public boolean isInStock() {
-		return this.inStock;
-	}
-
-	/**
-	 * @param inStock The isInStock to set.
-	 */
-	public void setInStock(boolean inStock) {
-		this.inStock = inStock;
-	}
-
-	/**
-	 * @param isbn The isbn to set.
-	 */
-	public void setIsbn(UUID isbn) {
-		this.isbn = isbn;
-	}
-
-	/**
-	 * @return Returns the title.
-	 */
-	public String getTitle() {
-		return this.title;
-	}
-
-	/**
-	 * @param title The title to set.
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	/**
-	 * @return Returns the author.
-	 */
-	public String getAuthor() {
-		return this.author;
-	}
-
-	/**
-	 * @param author The author to set.
-	 */
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	/**
-	 * @return Returns the pages.
-	 */
-	public int getPages() {
-		return this.pages;
-	}
-
-	/**
-	 * @param pages The pages to set.
-	 */
-	public void setPages(int pages) {
-		this.pages = pages;
-	}
-
-	@Override
-	public String toString() {
-		return ("isbn -> " + this.isbn) + "\n" + "tile -> " + this.title + "\n" + "author -> " + this.author
-				+ "\n" + "pages -> " + this.pages + "\n";
-	}
+public record Book(
+		@PrimaryKey UUID isbn,
+		String title,
+		@Indexed String author,
+		Integer pages,
+		LocalDate saleDate,
+		Boolean isInStock) {
 
 }
