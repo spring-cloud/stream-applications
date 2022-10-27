212ba6c860476612da60c3a689194fcf22de63af
Corneil du Plessis <pcorneil@vmware.com>
2022-10-27 15:32:31 +0200

    [GitHub Actions] Update machine type.

--- scripts/create-runners-cluster-gke.sh
+++ scripts/create-runners-cluster-gke.sh
@@ -7,13 +7,18 @@ if [ "$REGION" == "" ]; then
   export REGION="us-central1"
 fi
 
-# MACHINE_TYPE="e2-standard-8"
-export K8S="1.23"
+export K8S=$($SCDIR/determine-default.sh $CLUSTER_NAME "k8s_version")
 export MACHINE_TYPE=$($SCDIR/determine-default.sh $CLUSTER_NAME "machine_type")
+if [ "$MACHINE_TYPE" == "" ] || [ "$MACHINE_TYPE" == "null" ]; then
+  echo "Could not determine machine_type for $CLUSTER_NAME"
+fi
 export DISK_SIZE=$($SCDIR/determine-default.sh $CLUSTER_NAME "disk_size")
+if [ "$DISK_SIZE" == "" ] || [ "$DISK_SIZE" == "null" ]; then
+  echo "Could not determine disk_size for $CLUSTER_NAME"
+fi
 export NODES=1
 export PODS_PER_NODE=30
 
 
 echo "Creating stream-apps-gh-runners"
-$SCDIR/create-cluster-tf-gke.sh stream-apps-gh-runners
+$SCDIR/create-cluster-tf-gke.sh $CLUSTER_NAME
