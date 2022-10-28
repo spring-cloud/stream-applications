3ba45a0194ba190c8fe679b529d53a1832ae1318
Corneil du Plessis <pcorneil@vmware.com>
2022-10-28 13:31:35 +0200

    [GitHub Actions] Increase deployment timeout.

--- scripts/deploy-gh-runners.sh
+++ scripts/deploy-gh-runners.sh
@@ -81,7 +81,7 @@ if [ "$DEPLOY_TYPE" == "helm" ]; then
   echo "Adding Helm chart https://actions-runner-controller.github.io/actions-runner-controller"
   helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller
   echo "Installing application: actions-runner-controller, Helm chart version:$HELM_VER into $NS"
-  helm upgrade --install --version "$HELM_VER" --namespace $NS -f $SCDIR/arc/values.yml --wait actions-runner-controller actions-runner-controller/actions-runner-controller
+  helm upgrade --install --version "$HELM_VER" --namespace $NS -f $SCDIR/arc/values.yml --timeout 15m --wait actions-runner-controller actions-runner-controller/actions-runner-controller
 else
   ARC_VER=$($SCDIR/determine-default.sh stream-apps-gh-runners "arc_version")
   if [ "$ARC_VER" == "" ] || [ "$ARC_VER" == "null" ]; then
