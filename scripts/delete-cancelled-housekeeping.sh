a88b7100f53193f28584d77bcf73c589f7dc4806
Corneil du Plessis <pcorneil@vmware.com>
2022-11-01 21:11:09 +0200

    [GitHub Actions] Fix names

--- scripts/delete-cancelled-housekeeping.sh
+++ scripts/delete-cancelled-housekeeping.sh
@@ -7,11 +7,11 @@ RUNS=$(gh run list --limit 100 -w "housekeeping.yml" --json "conclusion,event,da
 
 for run in $RUNS; do
   echo "Deleting run $run"
-  gh api --method DELETE -H "Accept: application/vnd.github+json" "/repos/pivotal/scdf-pro/actions/runs/$run"
+  gh api --method DELETE -H "Accept: application/vnd.github+json" "/repos/spring-cloud/stream-applications/actions/runs/$run"
 done
 
 RUNS=$(gh run list --limit 100 -w "clean-housekeeping.yml" --json "conclusion,databaseId,status"  --jq 'map(select(.status == "completed" and .conclusion == "success")) | .[] | .databaseId' | sed 's/\"//g')
 for run in $RUNS; do
   echo "Deleting run $run"
-  gh api --method DELETE -H "Accept: application/vnd.github+json" "/repos/pivotal/scdf-pro/actions/runs/$run"
+  gh api --method DELETE -H "Accept: application/vnd.github+json" "/repos/spring-cloud/stream-applications/actions/runs/$run"
 done
--- scripts/delete-runners.sh
+++ scripts/delete-runners.sh
@@ -2,9 +2,9 @@
 SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 SCDIR=$(realpath $SCDIR)
 
-export CLUSTER_NAME="scdf-pro-gh-runners"
+export CLUSTER_NAME="stream-apps-gh-runners"
 set +e
-echo "Deleting scdf-pro-gh-runners"
+echo "Deleting stream-apps-gh-runners"
 RDEPLOY=$(kubectl get rdeploy --output=json | jq '.items | .[] | .metadata.name' | sed 's/\"//g')
 for item in $RDEPLOY; do
   kubectl delete rdeploy $item
