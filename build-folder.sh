f72d330dbf9780fed384c3215851fda693d306f5
Corneil du Plessis <pcorneil@vmware.com>
2022-11-08 16:23:55 +0200

    [GitHub Actions] Separate builds for folders

--- build-folder.sh
+++ build-folder.sh
@@ -18,10 +18,16 @@ function check_env() {
 }
 
 if [ "$VERBOSE" == "true" ]; then
-  MAVEN_OPTS="-s ./.settings.xml --debug -B -T 1C"
-else
-  MAVEN_OPTS="-s ./.settings.xml -B -T 1C"
+  MAVEN_OPTS="--debug"
+elif [ "$VERBOSE" == "false" ]; then
+  MAVEN_OPTS="-q"
 fi
+if [ "$MAVEN_OPTS" == "" ]; then
+  MAVEN_OPTS="-s ./.settings.xml -B -T 1C"
+else
+  MAVEN_OPTS="$MAVEN_OPTS -s ./.settings.xml -B -T 1C"
+fi
+
 if [ "$1" == "" ]; then
   echo -e "Options: ${bold} <folder> [<maven-goals>]*${end}"
 fi
@@ -35,6 +41,7 @@ fi
 SAVED_IFS=$IFS
 IFS=,
 FOLDERS=
+START_TIME=$(date +%s)
 for FOLDER in $FOLDER_NAMES; do
   if [ ! -d "$FOLDER" ]; then
     echo -e "${bold}Folder not found ${red}$FOLDER${end}"
@@ -54,9 +61,10 @@ if [[ "$MAVEN_GOAL" == *"deploy"* ]]; then
 fi
 
 RETRIES=3
-while ((RETRIES > 0)); do
+while ((RETRIES >= 0)); do
   set +e
   for FOLDER in $FOLDERS; do
+    echo -e "Resolving dependencies for ${bold}$FOLDER${end}"
     ./mvnw -U -f "$FOLDER" $MAVEN_OPTS dependency:resolve
   done
   set -e
@@ -65,40 +73,29 @@ while ((RETRIES > 0)); do
     break
   fi
   RETRIES=$((RETRIES - 1))
+  if ((RETRIES >= 0)); then
+    echo -e "RETRY:Resolving dependencies for ${bold}$FOLDER${end}"
+  fi
 done
-if [ "$MAVEN_GOAL" != "install" ]; then
+if ((RESULT == 0)); then
   for FOLDER in $FOLDERS; do
-    RETRIES=1
-    while ((RETRIES > 0)); do
-      echo -e "Maven goals:${bold}-f $FOLDER -DskipTests install${end}"
+    RETRIES=2
+    while ((RETRIES >= 0)); do
       set +e
-      ./mvnw -f "$FOLDER" -DskipTests $MAVEN_OPTS install
+      echo -e "Maven goals:${bold}-f $FOLDER $MAVEN_GOAL${end}"
+      ./mvnw -f "$FOLDER" $MAVEN_OPTS $MAVEN_GOAL
       set -e
       RESULT=$?
       if ((RESULT == 0)); then
         break
       fi
       RETRIES=$((RETRIES - 1))
-      if ((RETRIES > 0)); then
-        echo -e "RETRY:Maven goals:${bold}-f $FOLDER -DskipTests install${end}"
+      if ((RETRIES >= 0)); then
+        echo -e "RETRY:Maven goals:${bold}-f $FOLDER $MAVEN_GOAL${end}"
       fi
     done
   done
 fi
-for FOLDER in $FOLDERS; do
-  RETRIES=1
-  while ((RETRIES > 0)); do
-    set +e
-    echo -e "Maven goals:${bold}-f $FOLDER $MAVEN_GOAL${end}"
-    ./mvnw -f "$FOLDER" $MAVEN_OPTS $MAVEN_GOAL
-    set -e
-    RESULT=$?
-    if ((RESULT == 0)); then
-      break
-    fi
-    RETRIES=$((RETRIES - 1))
-    if ((RETRIES > 0)); then
-      echo -e "RETRY:Maven goals:${bold}-f $FOLDER $MAVEN_GOAL${end}"
-    fi
-  done
-done
+END_TIME=$(date +%s)
+DURATION=$((END_TIME - START_TIME))
+echo "Build of $FOLDER_NAMES $MAVEN_GOAL completed with result:$RESULT in $DURATION seconds"
