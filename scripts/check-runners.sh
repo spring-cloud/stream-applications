ef216fecbd5531dcdffde2886831e19955a9fe6b
Corneil du Plessis <pcorneil@vmware.com>
2022-11-08 22:59:13 +0200

    [GitHub Actions] Improve scaling runners

--- scripts/check-runners.sh
+++ scripts/check-runners.sh
@@ -15,5 +15,10 @@ echo ""
 echo "Runners:"
 RUNNER_DEPLOYMENTS=$(echo "$DEPLOYMENTS" | grep -F "runner" | awk '{print $1}')
 for deployment in $RUNNER_DEPLOYMENTS; do
-  kubectl get runners -l runner-deployment-name=$deployment --output=json | jq --arg deployment $deployment '.items | map(.status) | group_by(.phase,.ready) | map({ "deployment": $deployment, "count": length, "phase": .[0].phase})'
+  RUNNERS=$(kubectl get runners -l runner-deployment-name=$deployment --output=json | jq -c '.')
+  PHASES=$(echo "$RUNNERS" | jq '.items | map(.status.phase) | unique | .[]' | sed 's/\"//g')
+  for phase in $PHASES; do
+    # kubectl get runners -l runner-deployment-name=$deployment --output=json | jq --arg deployment $deployment '.items | map(.status) | group_by(.phase,.ready) | map({ "deployment": $deployment, "count": length, "phase": .[0].phase})'
+    echo "$RUNNERS"| jq --arg phase $phase --arg deployment $deployment '.items | map(.status) | .[] | select(.phase == $phase)' | jq --slurp --arg phase $phase --arg deployment $deployment 'group_by(.phase) | map({ "deployment": $deployment, "count": length, "phase": $phase})'
+  done
 done
