e305f6699c96b0d377e20bf09fac4a9bd17483c5
Corneil du Plessis <pcorneil@vmware.com>
2022-12-11 06:52:36 +0200

    Fix pushd / popd in app related scripts.

--- build-app.sh
+++ build-app.sh
@@ -32,74 +32,75 @@ pushd "$PROJECT_FOLDER" >/dev/null
   if [ "$VERSION" == "" ]; then
     VERSION=$($SCDIR/mvn-get-version.sh)
   fi
-popd >/dev/null
-echo "Project Version:$VERSION"
-if [ "$LOCAL" == "true" ]; then
-  MAVEN_GOAL="install verify"
-else
-  MAVEN_GOAL="install verify deploy"
-fi
 
-if [[ "$VERSION" == "4."* ]]; then
-  JDKS="17"
-  if [ "$DEFAULT_JDK" == "" ]; then
-    DEFAULT_JDK=17
-  fi
-else
-  JDKS="8 11 17"
-  if [ "$DEFAULT_JDK" == "" ]; then
-    DEFAULT_JDK=11
-  fi
-fi
-
-pushd "$APP_FOLDER" >/dev/null
-  rm -rf apps
-  echo "Deploying:$APP_FOLDER"
-  if [ -d "src/main/java" ]; then
-    echo "Deploying:$APP_FOLDER"
-    $SCDIR/build-folder.sh "." "$MAVEN_GOAL -Pintegration"
+  echo "Project Version:$VERSION"
+  if [ "$LOCAL" == "true" ]; then
+    MAVEN_GOAL="install verify"
   else
-    echo "Installing:$APP_FOLDER"
-    $SCDIR//build-folder.sh "." "clean install -Pintegration"
+    MAVEN_GOAL="install verify deploy"
   fi
 
-  if [ ! -d apps ]; then
-    echo "Cannot find $APP_FOLDER/apps"
-    exit 2
+  if [[ "$VERSION" == "4."* ]]; then
+    JDKS="17"
+    if [ "$DEFAULT_JDK" == "" ]; then
+      DEFAULT_JDK=17
+    fi
+  else
+    JDKS="8 11 17"
+    if [ "$DEFAULT_JDK" == "" ]; then
+      DEFAULT_JDK=11
+    fi
   fi
-  pushd apps >/dev/null
-    APPS=$(find * -maxdepth 0 -type d)
-    for app in $APPS; do
-      echo "Removing jib-maven-plugin for:$APP_FOLDER/apps/$app"
-      $SCDIR/scripts/remove-jib-plugin.sh $app/pom.xml
-    done
-    for app in $APPS; do
-      pushd "$app" >/dev/null
-        FOLDER=$(pwd)
-        echo "Building:$FOLDER"
-        $SCDIR/build-folder.sh "." "$MAVEN_GOAL -Pintegration"
-        for v in $JDKS; do
-          echo "Pack:$app:$VERSION-jdk$v"
-          pack build \
-            --path "target/$app-$VERSION.jar" \
-            --builder gcr.io/paketo-buildpacks/builder:base \
-            --env BP_JVM_VERSION=$v \
-            --env BPE_APPEND_JDK_JAVA_OPTIONS=-Dfile.encoding=UTF-8 \
-            --env BPE_APPEND_JDK_JAVA_OPTIONS=-Dsun.jnu.encoding \
-            --env BPE_LC_ALL=en_US.utf8 \
-            --env BPE_LANG=en_US.utf8 \
-            "springcloudstream/$app:$VERSION-jdk$v"
-          echo "Created:springcloudstream/$app:$VERSION-jdk$v"
-        done
-        if [ "$DEFAULT_JDK" != "" ]; then
-          docker tag "springcloudstream/$app:$VERSION-jdk$DEFAULT_JDK" "springcloudstream/$app:$VERSION"
-          echo "Tagged:springcloudstream/$app:$VERSION-jdk$DEFAULT_JDK as springcloudstream/$app:$VERSION"
-          if [ "$BRANCH" != "" ]; then
-            docker tag "springcloudstream/$app:$VERSION-jdk$DEFAULT_JDK" "springcloudstream/$app:$BRANCH"
-            echo "Tagged:springcloudstream/$app:$VERSION-jdk$DEFAULT_JDK as springcloudstream/$app:$BRANCH"
+
+  pushd "$APP_FOLDER" >/dev/null
+    rm -rf apps
+    echo "Deploying:$APP_FOLDER"
+    if [ -d "src/main/java" ]; then
+      echo "Deploying:$APP_FOLDER"
+      $SCDIR/build-folder.sh "." "$MAVEN_GOAL -Pintegration"
+    else
+      echo "Installing:$APP_FOLDER"
+      $SCDIR//build-folder.sh "." "clean install -Pintegration"
+    fi
+
+    if [ ! -d apps ]; then
+      echo "Cannot find $APP_FOLDER/apps"
+      exit 2
+    fi
+    pushd apps >/dev/null
+      APPS=$(find * -maxdepth 0 -type d)
+      for app in $APPS; do
+        echo "Removing jib-maven-plugin for:$APP_FOLDER/apps/$app"
+        $SCDIR/scripts/remove-jib-plugin.sh $app/pom.xml
+      done
+      for app in $APPS; do
+        pushd "$app" >/dev/null
+          FOLDER=$(pwd)
+          echo "Building:$FOLDER"
+          $SCDIR/build-folder.sh "." "$MAVEN_GOAL -Pintegration"
+          for v in $JDKS; do
+            echo "Pack:$app:$VERSION-jdk$v"
+            pack build \
+              --path "target/$app-$VERSION.jar" \
+              --builder gcr.io/paketo-buildpacks/builder:base \
+              --env BP_JVM_VERSION=$v \
+              --env BPE_APPEND_JDK_JAVA_OPTIONS=-Dfile.encoding=UTF-8 \
+              --env BPE_APPEND_JDK_JAVA_OPTIONS=-Dsun.jnu.encoding \
+              --env BPE_LC_ALL=en_US.utf8 \
+              --env BPE_LANG=en_US.utf8 \
+              "springcloudstream/$app:$VERSION-jdk$v"
+            echo "Created:springcloudstream/$app:$VERSION-jdk$v"
+          done
+          if [ "$DEFAULT_JDK" != "" ]; then
+            docker tag "springcloudstream/$app:$VERSION-jdk$DEFAULT_JDK" "springcloudstream/$app:$VERSION"
+            echo "Tagged:springcloudstream/$app:$VERSION-jdk$DEFAULT_JDK as springcloudstream/$app:$VERSION"
+            if [ "$BRANCH" != "" ]; then
+              docker tag "springcloudstream/$app:$VERSION-jdk$DEFAULT_JDK" "springcloudstream/$app:$BRANCH"
+              echo "Tagged:springcloudstream/$app:$VERSION-jdk$DEFAULT_JDK as springcloudstream/$app:$BRANCH"
+            fi
           fi
-        fi
-      popd >/dev/null
-    done
+        popd >/dev/null
+      done
+    popd >/dev/null
   popd >/dev/null
 popd >/dev/null
