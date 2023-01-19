f1f6f39649ef7511cee1d61817d6fae52ef645b6
Corneil du Plessis <corneil.duplessis@gmail.com>
2023-01-19 13:17:45 +0200

    Update routertest.groovy

--- applications/sink/router-sink/src/test/resources/routertest.groovy
+++ applications/sink/router-sink/src/test/resources/routertest.groovy
@@ -2,5 +2,5 @@ if ('foo' == headers.route) {
     return "$foo" // mapped to baz in 'variables'
 }
 else {
-    return "$bar" // mapped to qux in properties file.
+    return "$bar" // mapped to qux in properties file
 }
