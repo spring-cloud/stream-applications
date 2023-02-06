3ac1499001d6ea5a8340aba8aa8bbf85bfee44df
Corneil du Plessis <pcorneil@vmware.com>
2023-02-06 16:38:29 +0200

    Ensure k8s version is 1.23 using COS.

--- scripts/terraform/stream-apps-gh-runners/main.tf
+++ scripts/terraform/stream-apps-gh-runners/main.tf
@@ -131,9 +131,6 @@ resource "google_container_cluster" "primary" {
   }
   ip_allocation_policy {
   }
-  managed_prometheus {
-    enabled = false
-  }
 }
 
 resource "google_compute_firewall" "stream-ci-fw" {
