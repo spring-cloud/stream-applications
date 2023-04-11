d64a872fb5dfa84ed557f72ba0256f294d1a832c
Corneil du Plessis <pcorneil@vmware.com>
2023-04-11 15:21:30 +0200

    Print grep version

--- get-build-version-params.sh
+++ get-build-version-params.sh
@@ -9,7 +9,7 @@ fi
 
 # get the target release version and type
 export BUILD_VERSION=$(cat $SCDIR/version/RELEASE_VERSION)
-
+grep --version
 IS_SNAPSHOT=$(echo $BUILD_VERSION | grep -E "^.*-SNAPSHOT$")
 IS_MILESTONE=$(echo $BUILD_VERSION | grep -E "^.*-(M|RC)[0-9]+$")
 IS_GA=$(echo $BUILD_VERSION | grep -E "^.*\.[0-9]+$")
