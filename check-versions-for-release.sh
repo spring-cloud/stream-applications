688bc9bb9dfee85e50798fe49920b1b48bbe8b03
Chris Bono <cbono@vmware.com>
2023-04-12 00:02:04 -0500

    [CI] Release workflow (WIP)

--- check-versions-for-release.sh
+++ check-versions-for-release.sh
@@ -5,7 +5,6 @@ if [ "$1" == "" ]; then
   exit 1
 fi
 RELEASE_TYPE="$1"
-VERBOSE="$2"
 
 set +e
 
@@ -13,20 +12,18 @@ if [ "$RELEASE_TYPE" = "milestone" ]; then
   echo "Checking versions for $RELEASE_TYPE release"
   lines=$(find . -type f -name pom.xml | xargs grep SNAPSHOT | grep -v ".contains(" | grep -v regex | wc -l)
   if [ $lines -ne 0 ]; then
-    echo "Snapshots version found ($lines). Exiting build"
-    if [ "$VERBOSE" = "true" ]; then
-        echo $(find . -type f -name pom.xml | xargs grep SNAPSHOT | grep -v ".contains(" | grep -v regex)
-    fi
+    echo "Snapshots version found ($lines):"
+    echo $(find . -type f -name pom.xml | xargs grep SNAPSHOT | grep -v ".contains(" | grep -v regex)
+    echo "Exiting release build"
     exit 1
   fi
 elif [ "$RELEASE_TYPE" = "ga" ]; then
   echo "Checking versions for $RELEASE_TYPE release"
   lines=$(find . -type f -name pom.xml | xargs egrep "SNAPSHOT|M[0-9]|RC[0-9]" | grep -v ".contains(" | grep -v regex | wc -l)
   if [ $lines -ne 0 ]; then
-    echo "Non release versions found ($lines). Exiting build"
-    if [ "$VERBOSE" = "true" ]; then
-        echo $(find . -type f -name pom.xml | xargs egrep "SNAPSHOT|M[0-9]|RC[0-9]" | grep -v ".contains(" | grep -v regex)
-    fi
+    echo "Non release versions found ($lines):"
+    echo $(find . -type f -name pom.xml | xargs egrep "SNAPSHOT|M[0-9]|RC[0-9]" | grep -v ".contains(" | grep -v regex)
+    echo "Exiting release build"
     exit 1
   fi
 else
