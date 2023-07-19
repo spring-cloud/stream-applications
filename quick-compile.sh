7ebd3e75981b03213130adb3e3fdcd9ed2ae2847
Corneil du Plessis <pcorneil@vmware.com>
2023-07-19 17:22:57 +0200

    Fix the use of spring-cloud-dependencies in to ensure correct versions across tests and apps.

--- quick-compile.sh
+++ quick-compile.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 SCDIR=$(realpath $SCDIR)
-$SCDIR/build-core.sh "-T 0.5C install -P-snapshot -P-integration -DskipTests -q"
-$SCDIR/build-folder.sh ./applications/processor,./applications/sink,./applications/source,./applications/stream-applications-integration-tests,stream-applications-release-train "-T 0.5C install -P-snapshot -P-integration -DskipTests -q"
+export MAVEN_THREADS=1C
+export LOCAL=true
+$SCDIR/mvnw install -DskipTests -T 1C -P-snapshot -P-integration -DskipTests
