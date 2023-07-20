0398d354fb13d6b1a85d98712f1e223f1bf36032
Corneil du Plessis <pcorneil@vmware.com>
2023-07-20 14:04:18 +0200

    [CI] Fix update-project-version.sh

--- update-project-version.sh
+++ update-project-version.sh
@@ -7,36 +7,54 @@ if [ "$2" = "" ]; then
 fi
 VERSION=$1
 RELEASE_TRAIN_VERSION=$2
+OLD_VERSION=$(SCDIR/mvnw exec:exec -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive -q | sed 's/\"//g' | sed 's/version=//g')
+OLD_RT_VERSION=$(SCDIR/mvnw exec:exec -f stream-applications-release-train -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive -q | sed 's/\"//g' | sed 's/version=//g')
 $SCDIR/mvnw versions:set \
+  -DskipResolution=true \
   -DnewVersion="$VERSION" \
+  -DoldVersion="$OLD_VERSION" \
   -s .settings.xml \
   -DprocessAllModules=true \
   -DgenerateBackupPoms=false \
   -Dartifactory.publish.artifacts=false \
   -B $VERBOSE
 $SCDIR/mvnw versions:set-property -f applications/stream-applications-core \
+  -DskipResolution=true \
   -s .settings.xml \
   -DgenerateBackupPoms=false \
   -Dproperty=stream-apps-core.version \
   -DnewVersion="$VERSION" \
   -B $VERBOSE
 $SCDIR/mvnw versions:set-property -f applications/stream-applications-core \
+  -DskipResolution=true \
   -s .settings.xml \
   -DgenerateBackupPoms=false \
   -Dproperty=java-functions.version \
   -DnewVersion="$VERSION" \
   -B $VERBOSE
-$SCDIR/mvnw versions:set -f stream-applications-release-train \
-  -DnewVersion="$RELEASE_TRAIN_VERSION" \
-  -s .settings.xml \
-  -DprocessFromLocalAggregationRoot=false
-  -DprocessParent=false \
-  -DgenerateBackupPoms=false \
-  -Dartifactory.publish.artifacts=false \
-  -B $VERBOSE
-$SCDIR/mvnw versions:set-property -f stream-applications-release-train \
+
+# $SCDIR/mvnw install -DskipTests -o -T 1C
+$SCDIR/mvnw versions:set-property -pl :stream-applications-release-train \
+  -DskipResolution=true \
   -s .settings.xml \
   -DgenerateBackupPoms=false \
   -Dproperty=apps.version \
   -DnewVersion="$VERSION" \
+  -B $VERBOSE
+$SCDIR/mvnw versions:set -f stream-applications-release-train \
+  -DskipResolution=true \
+  -DoldVersion="$OLD_RT_VERSION" \
+  -DnewVersion="$RELEASE_TRAIN_VERSION" \
+  -s .settings.xml \
+  -DprocessFromLocalAggregationRoot=false \
+  -DprocessParent=false \
+  -DgenerateBackupPoms=false \
+  -Dartifactory.publish.artifacts=false \
+  -B $VERBOSE
+$SCDIR/mvnw versions:update-parent -pl :stream-applications-descriptor,:stream-applications-docs \
+  -DskipResolution=true \
+  -s .settings.xml \
+  -DgenerateBackupPoms=false \
+  -DparentVersion="$RELEASE_TRAIN_VERSION" \
+  -DallowSnapshots=true \
   -B $VERBOSE
