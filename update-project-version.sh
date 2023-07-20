dd6fd81f1c8ab2045c7c0b79a21a35f12817b9e5
Corneil du Plessis <pcorneil@vmware.com>
2023-07-20 16:03:01 +0200

    [CI] Fix update-project-version.sh

--- update-project-version.sh
+++ update-project-version.sh
@@ -5,10 +5,18 @@ if [ "$2" = "" ]; then
   echo "VERSION and RELEASE_TRAIN_VERSION required as arguments"
   exit 1
 fi
+function find_version() {
+    for v in $1; do
+      VER=$v
+    done
+    echo $VER
+}
 VERSION=$1
 RELEASE_TRAIN_VERSION=$2
-OLD_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout | sed 's/\"//g' | sed 's/version=//g')
-OLD_RT_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout -f stream-applications-release-train | sed 's/\"//g' | sed 's/version=//g')
+OLD_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout 2> /dev/null)
+OLD_VERSION=$(find_version "$OLD_VERSION")
+OLD_RT_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout -f stream-applications-release-train 2> /dev/null)
+OLD_RT_VERSION=$(find_version "$OLD_RT_VERSION")
 if [ "$VERBOSE" = "" ]; then
   VERBOSE=-q
 fi
@@ -17,7 +25,7 @@ echo "Release Train Version: [$OLD_RT_VERSION] -> [$RELEASE_TRAIN_VERSION]"
 
 $SCDIR/mvnw versions:set \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -Dartifactory.publish.artifacts=false -B $VERBOSE \
-  -DnewVersion="$VERSION" -DprocessAllModules=true
+  -DoldVersion="$OLD_VERSION" -DnewVersion="$VERSION" -DprocessAllModules=true
 
 $SCDIR/mvnw versions:set-property -f applications/stream-applications-core \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -B $VERBOSE \
@@ -26,16 +34,12 @@ $SCDIR/mvnw versions:set-property -f applications/stream-applications-core \
 $SCDIR/mvnw versions:set-property -f applications/stream-applications-core \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -B $VERBOSE \
   -Dproperty=java-functions.version -DnewVersion="$VERSION"
-
 $SCDIR/mvnw versions:set-property -pl :stream-applications-release-train \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -B $VERBOSE \
   -Dproperty=apps.version -DnewVersion="$VERSION" \
 
+echo "Update versions for stream-applications-release-train -> $RELEASE_TRAIN_VERSION"
 $SCDIR/mvnw versions:set -f stream-applications-release-train \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -Dartifactory.publish.artifacts=false -B $VERBOSE \
-  -DnewVersion="$RELEASE_TRAIN_VERSION" \
-  -DprocessFromLocalAggregationRoot=false -DprocessParent=false
+  -DoldVersion="$OLD_RT_VERSION" -DnewVersion="$RELEASE_TRAIN_VERSION" \
 
-$SCDIR/mvnw versions:update-parent -pl :stream-applications-descriptor,:stream-applications-docs \
-  -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -DgenerateBackupPoms=false -B $VERBOSE \
-  -DparentVersion="$RELEASE_TRAIN_VERSION" -DallowSnapshots=true
