f0c5792805c660d00ba43955d8e6f256a214cf53
Corneil du Plessis <pcorneil@vmware.com>
2023-07-24 11:42:19 +0200

    Fix next-dev-version.sh and versions.json

--- next-dev-version.sh
+++ next-dev-version.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 SCDIR=$(realpath $SCDIR)
-
+function update_field() {
+  FILE=$1
+  FIELD=$2
+  VALUE=$3
+  cat $FILE | jq --arg ndv "$VALUE" "$FIELD = \$ndv" > "$FILE.tmp"
+  CHECK=$(jq -r "$FIELD" "$FILE.tmp")
+  if [ "$CHECK" != "$VALUE" ]; then
+    echo "Expected $FIELD to be $VALUE not $CHECK"
+    exit 1
+  fi
+  cp -f "$FILE.tmp" "$FILE"
+  rm -f "$FILE.tmp"
+}
 set +e
 RELEASE_VERSION=$(jq -r '.release_version' ./versions.json)
 NEXT_DEV_VERSION=$(jq -r '.next_dev_version' ./versions.json)
 if [ "$RELEASE_VERSION" != "$NEXT_DEV_VERSION" ]; then
   echo "Updating RELEASE_VERSION: $RELEASE_VERSION to NEXT_DEV_VERSION: $NEXT_DEV_VERSION"
-  cat ./versions.json | jq --arg ndv "$NEXT_DEV_VERSION" '.release_version = $ndv' | tee ./versions.json
+  update_field ./versions.json ".release_version" $NEXT_DEV_VERSION
   RELEASE_VERSION=$(jq -r '.release_version' ./versions.json)
   echo "Updated RELEASE_VERSION: $RELEASE_VERSION"
 else
@@ -17,7 +29,7 @@ RELEASE_TRAIN_VERSION=$(jq -r '.release_train.release_version' ./versions.json)
 RELEASE_TRAIN_NEXT_DEV_VERSION=$(jq -r '.release_train.next_dev_version' ./versions.json)
 if [ "$RELEASE_TRAIN_VERSION" != "$RELEASE_TRAIN_NEXT_DEV_VERSION" ]; then
   echo "Updating RELEASE_TRAIN_VERSION: $RELEASE_TRAIN_VERSION to RELEASE_TRAIN_NEXT_DEV_VERSION: $RELEASE_TRAIN_NEXT_DEV_VERSION"
-  cat ./versions.json | jq --arg ndv "$RELEASE_TRAIN_NEXT_DEV_VERSION" '.release_train.release_version = $ndv' | tee ./versions.json
+  update_field ./versions.json ".release_train.release_version" "$RELEASE_TRAIN_NEXT_DEV_VERSION"
   RELEASE_TRAIN_VERSION=$(jq -r '.release_train.release_version' ./versions.json)
   echo "Updated RELEASE_TRAIN_VERSION: $RELEASE_TRAIN_VERSION"
 else
