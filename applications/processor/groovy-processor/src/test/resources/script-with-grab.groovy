4414a3b4ede2ecdb9d2d7a97732119c2846588fb
Artem Bilan <abilan@vmware.com>
2023-09-05 14:29:43 -0400

    Upgrade to latest Apache Ivy

--- applications/processor/groovy-processor/src/test/resources/script-with-grab.groovy
+++ applications/processor/groovy-processor/src/test/resources/script-with-grab.groovy
@@ -1,6 +1,4 @@
 @Grab('org.grooscript:grooscript:1.3.0')
 import org.grooscript.GrooScript
-@Grab('org.grooscript:grooscript:1.3.0')
-import org.grooscript.GrooScript
 
 GrooScript.convert(new String(payload))
