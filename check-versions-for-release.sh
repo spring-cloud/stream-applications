fa99e9f14fe30502760e95a8ac3f43308126b549
Corneil du Plessis <pcorneil@vmware.com>
2023-10-23 18:48:38 +0200

    [Release] 4.0.0 Release Train 2022.0.0

--- check-versions-for-release.sh
+++ check-versions-for-release.sh
@@ -13,16 +13,16 @@ if [ "$RELEASE_TYPE" = "milestone" ]; then
   lines=$(find . -type f -name pom.xml -not -path "./spring-cloud-dataflow-apps-plugin/*" | xargs grep SNAPSHOT | grep -v "SNAPSHOT</spring-cloud-dataflow-apps-" | grep -v ".contains(" | grep -v regex | wc -l)
   if [ $lines -ne 0 ]; then
     echo "Snapshots version found ($lines):"
-    echo $(find . -type f -name pom.xml | xargs grep SNAPSHOT | grep -v "SNAPSHOT</spring-cloud-dataflow-apps-" | grep -v ".contains(" | grep -v regex)
+    echo $(find . -type f -name pom.xml -not -path "./spring-cloud-dataflow-apps-plugin/*"  | xargs grep SNAPSHOT | grep -v "SNAPSHOT</spring-cloud-dataflow-apps-" | grep -v ".contains(" | grep -v regex)
     echo "Exiting release build"
     exit 1
   fi
 elif [ "$RELEASE_TYPE" = "ga" ]; then
   echo "Checking versions for $RELEASE_TYPE release"
-  lines=$(find . -type f -name pom.xml | xargs egrep "SNAPSHOT|M[0-9]|RC[0-9]" | grep -v ".contains(" | grep -v regex | wc -l)
+  lines=$(find . -type f -name pom.xml -not -path "./spring-cloud-dataflow-apps-plugin/*" | xargs egrep "SNAPSHOT|M[0-9]|RC[0-9]" | grep -v ".contains(" | grep -v regex | wc -l)
   if [ $lines -ne 0 ]; then
     echo "Non release versions found ($lines):"
-    echo $(find . -type f -name pom.xml | xargs egrep "SNAPSHOT|M[0-9]|RC[0-9]" | grep -v ".contains(" | grep -v regex)
+    echo $(find . -type f -name pom.xml -not -path "./spring-cloud-dataflow-apps-plugin/*" | xargs egrep "SNAPSHOT|M[0-9]|RC[0-9]" | grep -v ".contains(" | grep -v regex)
     echo "Exiting release build"
     exit 1
   fi
--- update-project-version.sh
+++ update-project-version.sh
@@ -34,16 +34,22 @@ $SCDIR/mvnw versions:set-property -f applications/stream-applications-core \
 $SCDIR/mvnw versions:set-property -f applications/stream-applications-core \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -B $VERBOSE \
   -Dproperty=java-functions.version -DnewVersion="$VERSION"
+
 $SCDIR/mvnw versions:set-property -pl :stream-applications-release-train \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -B $VERBOSE \
-  -Dproperty=apps.version -DnewVersion="$VERSION" \
+  -Dproperty=apps.version -DnewVersion="$VERSION"
 
+$SCDIR/mvnw install -pl :function-dependencies -am -DskipTests -T 1C
+
+OLD_RT_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout -f ./stream-applications-release-train 2> /dev/null)
+OLD_RT_VERSION=$(find_version "$OLD_RT_VERSION")
+echo "Release Train Version: [$OLD_RT_VERSION] -> [$RELEASE_TRAIN_VERSION]"
 echo "Update versions for stream-applications-release-train -> $RELEASE_TRAIN_VERSION"
-$SCDIR/mvnw versions:set -f stream-applications-release-train \
+$SCDIR/mvnw versions:set -f ./stream-applications-release-train \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -Dartifactory.publish.artifacts=false -B $VERBOSE \
-  -DnewVersion="$RELEASE_TRAIN_VERSION" \
+  -DoldVersion="$OLD_RT_VERSION" -DnewVersion="$RELEASE_TRAIN_VERSION"
 
-NEW_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout 2> /dev/null)
+NEW_VERSION=$($SCDIR/mvnw help:evaluate -DskipResolution=true -Dexpression=project.version -q -DforceStdout 2> /dev/null)
 NEW_VERSION=$(find_version "$NEW_VERSION")
 if [ "$NEW_VERSION" != "$VERSION" ]; then
   echo "Expected stream-applications version to be $VERSION not $NEW_VERSION"
@@ -52,7 +58,7 @@ fi
 echo "Version updated: stream-applications: $NEW_VERSION"
 PROJECTS="stream-applications-release-train stream-applications-descriptor stream-applications-docs"
 for proj in $PROJECTS; do
-  NEW_RT_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout -pl :$proj 2> /dev/null)
+  NEW_RT_VERSION=$($SCDIR/mvnw help:evaluate -DskipResolution=true -Dexpression=project.version -q -DforceStdout -pl :$proj 2> /dev/null)
   NEW_RT_VERSION=$(find_version "$NEW_RT_VERSION")
   if [ "$NEW_RT_VERSION" != "$RELEASE_TRAIN_VERSION" ]; then
     echo "Expected $proj version to be $RELEASE_TRAIN_VERSION not $NEW_RT_VERSION"
