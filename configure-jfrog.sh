36d55c1628dec726b3083d316ed5e341fdca7cee
Corneil du Plessis <pcorneil@vmware.com>
2023-10-24 15:55:59 +0200

    Provide for resolution of dependencies [skip ci]

--- configure-jfrog.sh
+++ configure-jfrog.sh
@@ -2,7 +2,7 @@
 MVNC=
 case $1 in
 "ga")
-  MVNC="--server-id-resolve=repo.spring.io --server-id-deploy=repo.spring.io --repo-resolve-releases=release --repo-resolve-snapshots=libs-snapshot --repo-deploy-releases=libs-release-local --repo-deploy-snapshots=libs-snapshot-local"
+  MVNC="--server-id-resolve=repo.spring.io --server-id-deploy=repo.spring.io --repo-resolve-releases=release --repo-resolve-snapshots=libs-snapshot --repo-deploy-releases=libs-staging-local --repo-deploy-snapshots=libs-snapshot-local"
   ;;
 "milestone")
   MVNC="--server-id-resolve=repo.spring.io --server-id-deploy=repo.spring.io --repo-resolve-releases=milestone --repo-resolve-snapshots=snapshot --repo-deploy-releases=libs-milestone-local --repo-deploy-snapshots=libs-snapshot-local"
