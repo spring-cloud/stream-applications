7a99b7bafbb487eb0d456c5538fa351443de696c
Corneil du Plessis <pcorneil@vmware.com>
2023-10-24 15:03:11 +0200

    Fix configure-jfrog.sh

--- configure-jfrog.sh
+++ configure-jfrog.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
+MVNC=
 case $1 in
 "ga")
-  jfrog mvnc \
-    --server-id-resolve=repo.spring.io \
-    --server-id-deploy=repo.spring.io \
-    --repo-resolve-releases=libs-release-local \
-    --repo-resolve-snapshots=libs-snapshot \
-    --repo-deploy-releases=libs-release-local \
-    --repo-deploy-snapshots=libs-snapshot-local
+  MVNC="--server-id-resolve=repo.spring.io --server-id-deploy=repo.spring.io --repo-resolve-releases=libs-release-local --repo-resolve-snapshots=libs-snapshot --repo-deploy-releases=libs-release-local --repo-deploy-snapshots=libs-snapshot-local"
   ;;
 "milestone")
-  jfrog mvnc \
-    --server-id-resolve=repo.spring.io \
-    --server-id-deploy=repo.spring.io \
-    --repo-resolve-releases=libs-milestone-local \
-    --repo-resolve-snapshots=libs-snapshot \
-    --repo-deploy-releases=libs-milestone-local \
-    --repo-deploy-snapshots=libs-snapshot-local
+  MVNC="--server-id-resolve=repo.spring.io --server-id-deploy=repo.spring.io --repo-resolve-releases=libs-milestone --repo-resolve-snapshots=libs-snapshot --repo-deploy-releases=libs-milestone-local --repo-deploy-snapshots=libs-snapshot-local"
   ;;
 "snapshot")
-  jfrog mvnc \
-    --server-id-resolve=repo.spring.io \
-    --server-id-deploy=repo.spring.io \
-    --repo-resolve-releases=libs-milestone \
-    --repo-resolve-snapshots=libs-snapshot \
-    --repo-deploy-releases=libs-milestone-local \
-    --repo-deploy-snapshots=libs-snapshot-local
+  MVNC="--server-id-resolve=repo.spring.io --server-id-deploy=repo.spring.io --repo-resolve-releases=libs-snapshot --repo-resolve-snapshots=libs-snapshot --repo-deploy-releases=libs-milestone-local --repo-deploy-snapshots=libs-snapshot-local"
   ;;
 *)
   echo "Invalid build type $1"
   exit 1
   ;;
 esac
-
+echo "MVNC=$MVNC"
+jfrog mvnc $MVNC
