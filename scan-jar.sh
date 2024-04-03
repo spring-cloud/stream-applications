8419a90769ef1444a347a05c0ce16407d91a0384
Corneil du Plessis <pcorneil@vmware.com>
2024-04-03 11:58:47 +0200

    Update logging scan-jar.sh

--- scan-jar.sh
+++ scan-jar.sh
@@ -4,6 +4,7 @@ SCDIR=$(realpath $SCDIR)
 if [[ "$1" != *"-sources.jar" ]] && [[ "$1" != *"-javadoc.jar" ]]; then
     if [ "$TRIVY_UPLOAD" == "true" ]; then
       echo "Scanning:$1"
+      echo "trivy rootfs --format sarif -o \"$1.sarif\" \"$1\""
       trivy rootfs --format sarif -o "$1.sarif" "$1"
       if [ -f "$1.sarif" ]; then
         if [ -f $SCDIR/runs.sarif ]; then
