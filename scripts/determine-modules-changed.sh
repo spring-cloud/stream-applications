f7d502b3952c1d9b953a7995fdc19872c196bf68
Chris Bono <cbono@vmware.com>
2024-11-07 21:53:05 -0600

    [CI] Restore change detection
    This commit restores the change detection that was
    unsuccessfully removed in previous commit.

--- scripts/determine-modules-changed.sh
+++ scripts/determine-modules-changed.sh
@@ -18,9 +18,25 @@ function addItem() {
 }
 MODIFIED="$*"
 ALL_MODULES=$(find . -name "pom.xml" -type f -exec dirname '{}' \; | sed 's/\.\///' | sort -r)
+MODULES=
+for file in $MODIFIED; do
+  FILE=$(realpath $file)
+  echo "$file was changed"
+  for ITEM in $ALL_MODULES; do
+    if [[ "$ITEM" != "." ]] && [[ "$file" == *"$ITEM"* ]]; then
+      echo "Matched $ITEM"
+      HAS_ITEM=$(itemInModules)
+      if ((HAS_ITEM == 0)); then
+        echo "Add:$ITEM"
+        MODULES=$(addItem "$ITEM")
+      fi
+      break
+    fi
+  done
+done
 echo "[" >modules.json
 COUNT=0
-for module in $ALL_MODULES; do
+for module in $MODULES; do
   if ((COUNT > 0)); then
     echo "," >> modules.json
   fi
