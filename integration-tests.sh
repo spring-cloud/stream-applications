0e0919076708b00806dfa1a544d48372f94d6e76
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2024-11-08 15:24:02 +0200

    Simplify ci-pr
    Removes checks for changes files to build only affected.
    
    See #550

--- integration-tests.sh
+++ integration-tests.sh
@@ -4,6 +4,5 @@ SCDIR=$(realpath $SCDIR)
 if [ "$BUILD_VERSION" = "" ]; then
   source $SCDIR/get-build-version-params.sh
 fi
-$SCDIR/build-core.sh "-T 1C install -Psnapshot -Pintegration"
-$SCDIR/build-folder.sh ./applications/processor,./applications/sink,./applications/source,./applications/stream-applications-integration-tests,stream-applications-release-train "-T 1C install -Psnapshot -Pintegration"
+set -e
 $SCDIR/build-folder.sh ./applications/stream-applications-integration-tests "verify -Dspring.cloud.stream.applications.version=${BUILD_VERSION} -Psnapshot -Pintegration"
