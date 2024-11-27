aad7b6f31c7bf78c958ff27c6fd60afc6cb02d7a
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2024-11-27 12:13:44 +0200

    Fix update-project-version.sh
    Fixes update-project-version.sh to change order of property updates.
    Ensure all scripts used are from 2022.0.x

--- mvn-get-version.sh
+++ mvn-get-version.sh
@@ -3,6 +3,6 @@ SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 SCDIR=$(realpath $SCDIR)
 VERSIONS=$($SCDIR/mvnw exec:exec -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive -q | sed 's/\"//g' | sed 's/version=//g')
 for v in $VERSIONS; do
-  VERSION=$v
+  MVN_VERSION=$v
 done
-echo "$VERSION"
+echo "$MVN_VERSION"
--- update-project-version.sh
+++ update-project-version.sh
@@ -11,7 +11,8 @@ function find_version() {
     done
     echo $VER
 }
-VERSION=$1
+./mvnw clean install -DskipTests -T 1C
+NEW_VERSION=$1
 RELEASE_TRAIN_VERSION=$2
 OLD_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout 2> /dev/null)
 OLD_VERSION=$(find_version "$OLD_VERSION")
@@ -20,45 +21,54 @@ OLD_RT_VERSION=$(find_version "$OLD_RT_VERSION")
 if [ "$VERBOSE" = "" ]; then
   VERBOSE=-q
 fi
-echo "Version:[$OLD_VERSION] -> [$VERSION]"
+echo "Version:[$OLD_VERSION] -> [$NEW_VERSION]"
 echo "Release Train Version: [$OLD_RT_VERSION] -> [$RELEASE_TRAIN_VERSION]"
+set -e
+
+$SCDIR/mvnw versions:set -f stream-applications-build \
+  -o -s .settings.xml -DgenerateBackupPoms=false -Dartifactory.publish.artifacts=false -B $VERBOSE \
+  -DoldVersion="$OLD_VERSION" -DnewVersion="$NEW_VERSION" -DprocessAllModules=false
+$SCDIR/mvnw install -DskipResolution=true -pl :stream-applications-build -DskipTests -T 1C
+
+$SCDIR/mvnw versions:set -f functions/function-dependencies \
+  -o -s .settings.xml -DgenerateBackupPoms=false -Dartifactory.publish.artifacts=false -B $VERBOSE \
+  -DoldVersion="$OLD_VERSION" -DnewVersion="$NEW_VERSION" -DprocessAllModules=false
+$SCDIR/mvnw install -DskipResolution=true -pl :function-dependencies -DskipTests -T 1C
 
 $SCDIR/mvnw versions:set \
-  -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -Dartifactory.publish.artifacts=false -B $VERBOSE \
-  -DoldVersion="$OLD_VERSION" -DnewVersion="$VERSION" -DprocessAllModules=true
+  -s .settings.xml -DgenerateBackupPoms=false -Dartifactory.publish.artifacts=false -B $VERBOSE \
+  -DoldVersion="$OLD_VERSION" -DnewVersion="$NEW_VERSION" -DprocessAllModules=true
 
 $SCDIR/mvnw versions:set-property -f applications/stream-applications-core \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -B $VERBOSE \
-  -Dproperty=stream-apps-core.version -DnewVersion="$VERSION"
+  -Dproperty=stream-apps-core.version -DnewVersion="$NEW_VERSION"
 
 $SCDIR/mvnw versions:set-property -f applications/stream-applications-core \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -B $VERBOSE \
-  -Dproperty=java-functions.version -DnewVersion="$VERSION"
+  -Dproperty=java-functions.version -DnewVersion="$NEW_VERSION"
 
 $SCDIR/mvnw versions:set-property -pl :stream-applications-release-train \
   -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -B $VERBOSE \
-  -Dproperty=apps.version -DnewVersion="$VERSION"
+  -Dproperty=apps.version -DnewVersion="$NEW_VERSION"
 
-$SCDIR/mvnw install -pl :function-dependencies -am -DskipTests -T 1C
-
-OLD_RT_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout -f ./stream-applications-release-train 2> /dev/null)
+OLD_RT_VERSION=$($SCDIR/mvnw help:evaluate -DskipResolution=true  -Dexpression=project.version -q -DforceStdout -f ./stream-applications-release-train 2> /dev/null)
 OLD_RT_VERSION=$(find_version "$OLD_RT_VERSION")
 echo "Release Train Version: [$OLD_RT_VERSION] -> [$RELEASE_TRAIN_VERSION]"
 echo "Update versions for stream-applications-release-train -> $RELEASE_TRAIN_VERSION"
 $SCDIR/mvnw versions:set -f ./stream-applications-release-train \
-  -DskipResolution=true -s .settings.xml -DgenerateBackupPoms=false -Dartifactory.publish.artifacts=false -B $VERBOSE \
+  -DskipResolution=true  -s .settings.xml -DgenerateBackupPoms=false -Dartifactory.publish.artifacts=false -B $VERBOSE \
   -DoldVersion="$OLD_RT_VERSION" -DnewVersion="$RELEASE_TRAIN_VERSION"
 
-NEW_VERSION=$($SCDIR/mvnw help:evaluate -DskipResolution=true -Dexpression=project.version -q -DforceStdout 2> /dev/null)
-NEW_VERSION=$(find_version "$NEW_VERSION")
-if [ "$NEW_VERSION" != "$VERSION" ]; then
-  echo "Expected stream-applications version to be $VERSION not $NEW_VERSION"
+FOUND_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout 2> /dev/null)
+FOUND_VERSION=$(find_version "$FOUND_VERSION")
+if [ "$NEW_VERSION" != "$FOUND_VERSION" ]; then
+  echo "Expected stream-applications version to be $NEW_VERSION not $FOUND_VERSION"
   exit 1
 fi
-echo "Version updated: stream-applications: $NEW_VERSION"
+echo "Version updated: stream-applications: $FOUND_VERSION"
 PROJECTS="stream-applications-release-train stream-applications-descriptor stream-applications-docs"
 for proj in $PROJECTS; do
-  NEW_RT_VERSION=$($SCDIR/mvnw help:evaluate -DskipResolution=true -Dexpression=project.version -q -DforceStdout -pl :$proj 2> /dev/null)
+  NEW_RT_VERSION=$($SCDIR/mvnw help:evaluate -Dexpression=project.version -q -DforceStdout -pl :$proj 2> /dev/null)
   NEW_RT_VERSION=$(find_version "$NEW_RT_VERSION")
   if [ "$NEW_RT_VERSION" != "$RELEASE_TRAIN_VERSION" ]; then
     echo "Expected $proj version to be $RELEASE_TRAIN_VERSION not $NEW_RT_VERSION"
