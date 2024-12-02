6fdd6241f708631d7ca93eeebfeab984f097acb7
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2024-12-02 11:47:18 +0200

    Rollback to previous snapshot for improved build [skip ci]

--- update-project-version.sh
+++ update-project-version.sh
@@ -26,9 +26,9 @@ echo "Version:[$OLD_VERSION] -> [$NEW_VERSION]"
 echo "Release Train Version: [$OLD_RT_VERSION] -> [$RELEASE_TRAIN_VERSION]"
 set +e
 
-$SCDIR/mvnw clean install -DskipTests -T 1C -ntp -Dmaven.javadoc.skip=true
-$SCDIR/mvnw clean
-find $SCDIR -name apps -type d -exec rm -rf '{}' \;
+#$SCDIR/mvnw clean install -DskipTests -T 1C -ntp -Dmaven.javadoc.skip=true
+#$SCDIR/mvnw clean
+#find $SCDIR -name apps -type d -exec rm -rf '{}' \;
 
 $SCDIR/mvnw versions:set \
   -s .settings.xml -DgenerateBackupPoms=false -Dartifactory.publish.artifacts=false -B $VERBOSE \
