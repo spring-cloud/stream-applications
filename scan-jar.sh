3a8c3efeb8cdfbb6afd972c5230808ec16633dd4
Chris Bono <chris.bono@broadcom.com>
2025-01-30 19:26:54 -0600

    Update ivy version to 2.5.2 (#619)
    - Updates `org.apache.ivy:ivy` to version 2.5.2 to fix
    CVE-2022-46751
    
    - Adds 2 CVEs to trivyignore due to `debezium-supplier`
    transitive dependencies.
    
    (cherry picked from commit 6cc9cdde21a46c8198219862347422e82fa13638)

--- scan-jar.sh
+++ scan-jar.sh
@@ -4,8 +4,7 @@ SCDIR=$(realpath $SCDIR)
 if [[ "$1" != *"-sources.jar" ]] && [[ "$1" != *"-javadoc.jar" ]]; then
     if [ "$TRIVY_UPLOAD" == "true" ]; then
       echo "Scanning:$1"
-      echo "trivy rootfs --format sarif -o \"$1.sarif\" \"$1\""
-      trivy rootfs --format sarif -o "$1.sarif" "$1"
+      trivy rootfs --exit-code 1 --format sarif -o "$1.sarif" "$1"
       if [ -f "$1.sarif" ]; then
         if [ -f $SCDIR/runs.sarif ]; then
           echo "," >> "$SCDIR/runs.sarif"
@@ -15,7 +14,7 @@ if [[ "$1" != *"-sources.jar" ]] && [[ "$1" != *"-javadoc.jar" ]]; then
         echo "Could not find:$1.sarif"
       fi
     else
-      trivy rootfs -q "$1"
+      trivy rootfs --exit-code 1 -q "$1"
     fi
 else
   if [ "$TRIVY_UPLOAD" == "true" ]; then
