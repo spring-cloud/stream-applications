01812f5809110d29b2dbe2a526a9bc943ee21c74
Max Brauer <mbrauer@vmware.com>
2025-12-16 14:44:06 +0100

    ci: run full build and tests with the Stream Apps's version
    Signed-off-by: Max Brauer <mbrauer@vmware.com>

--- full-build-test.sh
+++ full-build-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 SCDIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 SCDIR=$(realpath $SCDIR)
-$SCDIR/build-core.sh "-T 0.5C install -Psnapshot -Pintegration"
-$SCDIR/build-folder.sh ./applications/processor,./applications/sink,./applications/source,./applications/stream-applications-integration-tests,stream-applications-release-train "-T 0.5C install -Psnapshot -Pintegration"
+: "${STREAM_APPS_VERSION:=$($SCDIR/mvn-get-version.sh)}"
+$SCDIR/build-core.sh "-T 0.5C install -Psnapshot -Pintegration -Dspring.cloud.stream.applications.version=$STREAM_APPS_VERSION"
+$SCDIR/build-folder.sh ./applications/processor,./applications/sink,./applications/source,./applications/stream-applications-integration-tests,stream-applications-release-train "-T 0.5C install -Psnapshot -Pintegration -Dspring.cloud.stream.applications.version=$STREAM_APPS_VERSION"
